82c2f2e298268ad6c648baae7404a107c44c70e0
Amiscript <edisonsime99@gmail.com>
2026-05-01 21:41:06 +0100

    2026-01-05

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Home from "./component/Home"
+import AboutUs from './component/AboutUs'
+
+function App() {
+
+
+  return (
+    <div>
+      <Home/>
+      <AboutUs/>
+    </div>
+  )
+}
+
+export default App
